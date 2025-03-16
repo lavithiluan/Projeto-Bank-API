abbc45b0fef751f5a99e563fffcc6d853f4ae526
Lavithiluan <thiago.henrydias@gmail.com>
2025-03-16 15:55:06 -0300

    bank

--- src/main/java/com/edu/bank/BankApplication.java
+++ src/main/java/com/edu/bank/BankApplication.java
@@ -1,5 +1,8 @@
 package com.edu.bank;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,5 +12,25 @@ public class BankApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BankApplication.class, args);
 	}
+@RestController
+@RequestMapping("/")
+class HomeController {
+    @GetMapping
+    public String home() {
+        return "Banco Digital API - Equipe: João, Maria, Pedro";
+    }
+}
+
+@RestController
+@RequestMapping("/contas")
+class ContaController {
+    private final List<Conta> contas = new ArrayList<>();
+
+    @PostMapping
+    public Conta cadastrarConta(@Valid @RequestBody Conta novaConta) {
+        contas.add(novaConta);
+        return novaConta;
+    }
+}
 
 }
---
+++ src/main/java/com/edu/bank/Conta.java
@@ -0,0 +1,43 @@
+package com.edu.bank;
+
+public class Conta {
+
+    private Double numero;
+    private Double agencia;
+    private String nomeTitular;
+    private String cpfTitular;
+    private String dataAbertura;
+    private Double saldoInicial;
+    private Boolean ativo;
+    private String tipo;
+    
+    public Double getNumero() {
+        return numero;
+    }
+    public Double getAgencia() {
+        return agencia;
+    }
+    public String getNomeTitular() {
+        return nomeTitular;
+    }
+    public String getCpfTitular() {
+        return cpfTitular;
+    }
+    public String getDataAbertura() {
+        return dataAbertura;
+    }
+    public Double getSaldoInicial() {
+        return saldoInicial;
+    }
+    public Boolean getAtivo() {
+        return ativo;
+    }
+    public String getTipo() {
+        return tipo;
+    }
+
+    
+
+
+    
+}
